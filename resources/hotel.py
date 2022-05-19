75bb5274afdd9102e07c0879f22108dd93a32f46
Weslley Rodrigo <weslley.rodriigo@gmail.com>
2022-05-19 20:46:44 -0300

    Implement new endpoint

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,8 +1,7 @@
 
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-
-
+import time
 
 class Hoteis(Resource):
     def get(self):
@@ -25,14 +24,25 @@ class Hotel(Resource):
             return hotel.json()
         return {'message': 'Hotel not found.'}, 400
 
-    def post(self, hotel_id):
-        if HotelModel.find_hotel(hotel_id):
-            return {"message": "Hotel id '{}' already exists." .format(hotel_id)},  #400bad request
+    # def post(self, hotel_id):
+    #     if HotelModel.find_hotel(hotel_id):
+    #         return {"message": "Hotel id '{}' already exists." .format(hotel_id)},  #400bad request
             
+    #     dados = Hotel.argumentos.parse_args()
+    #     if dados.get('nome').strip() == '' :
+    #         return{'messagem': 'Os campos nao podem ficar em branco.'}
+    #     hotel = HotelModel(hotel_id, **dados)
+    #     try:
+    #         hotel.save_hotel()
+    #     except:
+    #         return{'message': 'Error'} , 500 #internel serve
+    #     return hotel.json()
+
+    def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        if dados.get('nome').strip() == '' :
-            return{'messagem': 'Os campos nao podem ficar em branco.'}
-        hotel = HotelModel(hotel_id, **dados)
+
+        hotel = HotelModel(time.time(), **dados)
+
         try:
             hotel.save_hotel()
         except:
